34fc7b9dce089d2199f766b5448ef2d7d66fe536
Nathan Smith <nsmith@lcs.on.ca>
2021-12-03 11:09:07 -0500

    Initial Commit

---
+++ TemperatureConverterSwitchStatement/TemperatureConverterSwitchStatementApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConverterSwitchStatementApp.swift
+//  TemperatureConverterSwitchStatement
+//
+//  Created by Nate S on 2021-12-03.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConverterSwitchStatementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
