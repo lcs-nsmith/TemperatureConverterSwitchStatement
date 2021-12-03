7746f76dbfe308966e97a50a905b610460cc9629
Nathan Smith <nsmith@lcs.on.ca>
2021-12-03 11:40:10 -0500

    Example of using an If statement to ?visud? ?rge? need for logical AND operator to categorize a value within a range This works becuase machine is matched

--- TemperatureConverterSwitchStatement/ContentView.swift
+++ TemperatureConverterSwitchStatement/ContentView.swift
@@ -26,15 +26,15 @@ struct ContentView: View {
     }
     
     var vetementSuggestions: String {
-        if temperatureInCelsius >= -50.0 && temperatureInCelsius < -20.0 {
+        if temperatureInCelsius < -20.0 {
             return "It's freezing outside! 🥶"
-        } else if temperatureInCelsius >= -20.0 && temperatureInCelsius < 0.0 {
+        } else if temperatureInCelsius < 0.0 {
             return "Dress warmly!"
-        } else if temperatureInCelsius >= 0.0 && temperatureInCelsius < 10.0 {
+        } else if temperatureInCelsius < 10.0 {
             return "Need a coat!"
-        } else if temperatureInCelsius >= 10.0 && temperatureInCelsius < 20.0 {
+        } else if temperatureInCelsius < 20.0 {
             return "Not very cold, not very hot... Perfect!"
-        } else if temperatureInCelsius >= 20.0 && temperatureInCelsius < 30.0 {
+        } else if temperatureInCelsius < 30.0 {
             return "Warm weather! Wear thin layers."
         } else {
             return "Never go out! It's too hot. 🥵"
